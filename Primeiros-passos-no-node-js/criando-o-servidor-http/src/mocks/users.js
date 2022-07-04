5ba1de058505ed308e5156e0643def054597bf68
MoisesST <moises.dev13@gmail.com>
2022-07-04 14:05:59 -0300

    Primeiros passos no Node.js: Criando a primeira rota da API

---
+++ Primeiros-passos-no-node-js/criando-o-servidor-http/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Mateus',
+    },
+    {
+        id: 1,
+        name: 'Rose',
+    },
+    {
+        id: 1,
+        name: 'José',
+    },
+];
