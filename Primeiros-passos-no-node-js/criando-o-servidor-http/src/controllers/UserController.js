7797c0c760a4574e1abd9d6962cf433f2ccc991d
MoisesST <moises.dev13@gmail.com>
2022-07-05 21:17:10 -0300

    Primeiros passos no Node.js: Capturando o body das requests

--- Primeiros-passos-no-node-js/criando-o-servidor-http/src/controllers/UserController.js
+++ Primeiros-passos-no-node-js/criando-o-servidor-http/src/controllers/UserController.js
@@ -11,7 +11,7 @@ module.exports = {
 
             return a.id > b.id ? 1 : -1;
         });
-        
+
         response.send(200, sortedUsers);
     },
 
@@ -25,5 +25,27 @@ module.exports = {
         }
 
         response.send(200, user);
-    }
+    },
+
+    createUser(request, response) {
+        let body = '';
+
+        request.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        request.on('end', () => {
+            body = JSON.parse(body);
+
+            const lastUserId = users[users.length - 1].id;
+            const newUser = {
+                id: lastUserId + 1,
+                name: body.name,
+             };
+
+            users.push(newUser);
+
+            response.send(200, newUser);
+        });
+    },
 };
